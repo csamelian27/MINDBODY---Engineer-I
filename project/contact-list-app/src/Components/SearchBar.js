62f3a47728f48e72f0b51dd0e6699d46b4df7c63
csamelian27 <csamelian@gmail.com>
2019-07-14 23:36:25 -0400

    working on search bar

--- project/contact-list-app/src/Components/SearchBar.js
+++ project/contact-list-app/src/Components/SearchBar.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-const SearchBar = (props) => {
-  return (
-    <div>
-      <input type="text" placeholder="Search Your Contacts"></input>
-    </div>
-  )
+class SearchBar extends React.Component {
+
+  state = {
+    searchInput: ""
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+      searchInput: e.target.value
+    })
+    this.props.renderSearchCards(this.state.searchInput)
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder=" Search Your Contacts" value={this.state.searchInput} onChange={this.handleSearch}></input>
+      </div>
+    )
+  }
 }
 
 export default SearchBar;
--- project/contact-list-app/src/Containers/ContactContainer.js
+++ project/contact-list-app/src/Containers/ContactContainer.js
@@ -8,10 +8,21 @@ const ContactContainer = (props) => {
     return props.allContacts.map((contactObj, index) => <ContactCard key={index} contact={contactObj} handleUpdateContact={props.handleUpdateContact} handleDeleteContact={props.handleDeleteContact} />)
   }
 
+  const renderSearchCards = (searchInput) => {
+    let newArray = props.allContacts.filter(contact => {
+      console.log(Object.values(contact));
+      if(Object.values(contact).includes(searchInput)) {
+        return contact
+      }
+    })
+    console.log(newArray);
+  }
+
   console.log(props);
   return (
     <div className="contact-container">
       <div className="contact-container-header">Your Contacts:</div>
+      <SearchBar renderSearchCards={renderSearchCards} />
       {renderCards()}
     </div>
   )
